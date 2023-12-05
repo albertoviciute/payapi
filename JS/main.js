ad617a29d3a5593951ba29af1b08bd0045863b67
EvelinaNo <evelinavalickaja@gmail.com>
2023-12-05 14:52:45 +0200

    add hamburger on mobile home page

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,21 @@ function checkIfInputIsEmpty(element){
 function redirectToAboutPage(){
     window.location.href = 'about.html';
 }
+
+function showMobileMenu(){
+    document.getElementById("mobileDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-mobile')) {
+  
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
